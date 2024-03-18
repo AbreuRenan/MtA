e3e8c7f44ad0ec0c1e0e4ac74b5c054567860cc9
AbreuRenan <renan.de.abreu@gmail.com>
2024-03-18 16:35:26 -0300

    pwa ajustes

--- src/components/auth/LoginScreen.jsx
+++ src/components/auth/LoginScreen.jsx
@@ -95,21 +95,15 @@ window.addEventListener("beforeinstallprompt", (event) => {
   event.preventDefault();
 });
 
-// Lidar com o clique no botão de instalação
 document.getElementById("installButton").addEventListener("click", () => {
-  // Verificar se existe um evento de instalação pendente
-  console.log('ia')
   if (deferredPrompt) {
-    // Mostrar a solicitação de instalação
     deferredPrompt.prompt();
-    // Aguardar a resposta do usuário
     deferredPrompt.userChoice.then((choiceResult) => {
       if (choiceResult.outcome === "accepted") {
         console.log("Usuário aceitou a instalação");
       } else {
         console.log("Usuário recusou a instalação");
       }
-      // Limpar o evento de instalação pendente
       deferredPrompt = null;
     });
   }
